765103377efd7772729510fd0577eb6f5547bb64
Riffath Yousf <133844323+riffath-mm@users.noreply.github.com>
2024-01-17 19:45:32 +0530

    Add files via upload

---
+++ mergeSort.js
@@ -0,0 +1,20 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+function merge(left, right) {
+    let sortedArr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+    return [...sortedArr, ...left, ...right];
+}
+let arr = [8, 3, 5, 4, 7, 6, 1, 2];
+console.log(mergeSort(arr));
